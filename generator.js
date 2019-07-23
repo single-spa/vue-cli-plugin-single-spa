d9c512edd8d6480459684d1e308391c2d20c8622
Joel Denning <joeldenning@gmail.com>
2019-07-23 11:20:21 -0600

    Implementing vue-cli generator.

---
+++ generator.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = api => {
+  const isTs = api.entryFile.endsWith('.ts')
+
+  api.render({
+    [api.entryFile]: './template/src/main.js'
+  }, {
+    isTs,
+  })
+
+  api.extendPackage({
+    dependencies: {
+      'single-spa-vue': '^1.5.2'
+    }
+  })
+}
---
+++ template/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import App from './App.vue';
+import singleSpaVue from 'single-spa-vue';
+
+Vue.config.productionTip = false;
+
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render: (h<% if(isTs) {%>: any<% } %>) => h(App),
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
