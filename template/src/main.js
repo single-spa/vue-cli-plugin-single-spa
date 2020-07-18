7f83d7a77dccb1817fd38c8f96767bf1f3deaf1c
Joel Denning <joeldenning@gmail.com>
2020-07-18 10:30:32 -0600

    Making things more obvious

--- template/src/main.js
+++ template/src/main.js
@@ -14,7 +14,10 @@ const vueLifecycles = singleSpaVue({
       return h(App, {
         props: {
           // single-spa props are available on the "this" object. Forward them to your component as needed.
-          mountParcel: this.mountParcel
+          // https://single-spa.js.org/docs/building-applications#lifecyle-props
+          name: this.name,
+          mountParcel: this.mountParcel,
+          singleSpa: this.singleSpa,
         },
       });
     },<% if (usesRouter) { %>
