5781816c61978bee296561ea3b5aef68ffa62ed7
Joel Denning <joeldenning@gmail.com>
2020-07-20 10:43:39 -0600

    Making it more obvious how to use single-spa props. (#11)
    * Making it more obvious how to use single-spa props.
    
    * More docs
    
    * More things
    
    * Making things more obvious

--- template/src/main.js
+++ template/src/main.js
@@ -10,7 +10,17 @@ Vue.config.productionTip = false;
 const vueLifecycles = singleSpaVue({
   Vue,
   appOptions: {
-    render: (h<% if(isTs) {%>: any<% } %>) => h(App),<% if (usesRouter) { %>
+    render(h<% if(isTs) {%>: any<% } %>) {
+      return h(App, {
+        props: {
+          // single-spa props are available on the "this" object. Forward them to your component as needed.
+          // https://single-spa.js.org/docs/building-applications#lifecyle-props
+          name: this.name,
+          mountParcel: this.mountParcel,
+          singleSpa: this.singleSpa,
+        },
+      });
+    },<% if (usesRouter) { %>
     router,<% } %>
   },
 });
