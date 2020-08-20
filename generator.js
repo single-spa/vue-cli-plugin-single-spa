3402d81ddc0bd492107c6b02ae6b3086bc08e553
Joel Denning <joeldenning@gmail.com>
2020-08-20 15:37:46 -0600

    Support for Vue 3.

--- generator.js
+++ generator.js
@@ -1,11 +1,19 @@
+const semver = require('semver')
+
 module.exports = api => {
   const isTs = api.entryFile.endsWith('.ts')
   const { dependencies, name } = require(api.resolve('package.json'));
   const usesRouter = Boolean(dependencies && dependencies['vue-router']);
   const appName = name || 'appName';
+  const vueVersion = dependencies.vue
+  if (!vueVersion) {
+    throw Error(`Could not find vue dependency in package.json`)
+  }
+  const minVueVersion = semver.minVersion(vueVersion)
+  const isVue2 = semver.satisfies(minVueVersion, '<3')
 
   api.render({
-    [api.entryFile]: './template/src/main.js',
+    [api.entryFile]: `./template/src/main-vue-${isVue2 ? '2' : '3'}.js`,
     './src/set-public-path.js': './template/src/set-public-path.js',
   }, {
     isTs,
@@ -15,8 +23,8 @@ module.exports = api => {
 
   api.extendPackage({
     dependencies: {
-      'single-spa-vue': '^1.5.2',
-      'systemjs-webpack-interop': '^1.1.0',
+      'single-spa-vue': '^1.9.0',
+      'systemjs-webpack-interop': '^2.1.2',
     }
   })
 }
