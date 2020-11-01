ae051cee50624f3d20e29c8c743dc90d84216322
Joel Denning <joeldenning@gmail.com>
2020-11-01 11:19:02 -0700

    Remove unnecessary props property

--- template/src/main-vue-3.js
+++ template/src/main-vue-3.js
@@ -10,13 +10,11 @@ const vueLifecycles = singleSpaVue({
   appOptions: {
     render() {
       return h(App, {
-        props: {
-          // single-spa props are available on the "this" object. Forward them to your component as needed.
-          // https://single-spa.js.org/docs/building-applications#lifecyle-props
-          name: this.name,
-          mountParcel: this.mountParcel,
-          singleSpa: this.singleSpa,
-        },
+        // single-spa props are available on the "this" object. Forward them to your component as needed.
+        // https://single-spa.js.org/docs/building-applications#lifecyle-props
+        name: this.name,
+        mountParcel: this.mountParcel,
+        singleSpa: this.singleSpa,
       });
     },
   },<% if (usesRouter) { %>
