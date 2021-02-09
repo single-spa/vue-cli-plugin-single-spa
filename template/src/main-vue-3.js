f5a9be55ff8ff6d388131f800d91a2fc64464668
Andrew de Quincey <adq@lidskialf.net>
2021-02-09 17:38:07 +0000

    comment out props and add comment to add definitions (#26)

--- template/src/main-vue-3.js
+++ template/src/main-vue-3.js
@@ -11,9 +11,12 @@ const vueLifecycles = singleSpaVue({
       return h(App, {
         // single-spa props are available on the "this" object. Forward them to your component as needed.
         // https://single-spa.js.org/docs/building-applications#lifecyle-props
+        // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
+        /*
         name: this.name,
         mountParcel: this.mountParcel,
         singleSpa: this.singleSpa,
+        */
       });
     },
   },<% if (usesRouter) { %>
