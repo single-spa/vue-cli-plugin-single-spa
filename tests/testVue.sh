000a3df27e4502ca24430564a36ab0044be73051
Joel Denning <joeldenning@gmail.com>
2025-02-04 12:30:14 -0700

    Try to fix

--- tests/testVue.sh
+++ tests/testVue.sh
@@ -12,13 +12,13 @@ then
   fi
 
   echo "creating vue project"
-  pnpx @vue/cli create $ProjectName --no-git --inlinePreset "{\"useConfigFiles\": true,\"plugins\": {},\"vueVersion\": \"$1\"}" --packageManager=pnpm || ERRCODE=$?
+  npx @vue/cli create $ProjectName --no-git --inlinePreset "{\"useConfigFiles\": true,\"plugins\": {},\"vueVersion\": \"$1\"}" --packageManager=pnpm || ERRCODE=$?
 
   cd $ProjectName
   echo "installing local vue-cli-plugin-single-spa"
   pnpm install -D ../../.. || ERRCODE=$?
   echo "invoking vue-cli-plugin-single-spa"
-  yes | pnpx @vue/cli invoke single-spa || ERRCODE=$?
+  yes | npx @vue/cli invoke single-spa || ERRCODE=$?
   echo "creating vue.config.js"
   echo "module.exports={pluginOptions: {'single-spa': {outputSystemJS: $2}}}" > vue.config.js
   echo "building"
