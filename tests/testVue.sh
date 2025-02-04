b7d2c3fb4337988aea350d08cf44172fd5984de7
Joel Denning <joeldenning@gmail.com>
2025-02-04 12:31:20 -0700

    Try npm

--- tests/testVue.sh
+++ tests/testVue.sh
@@ -12,17 +12,17 @@ then
   fi
 
   echo "creating vue project"
-  npx @vue/cli create $ProjectName --no-git --inlinePreset "{\"useConfigFiles\": true,\"plugins\": {},\"vueVersion\": \"$1\"}" --packageManager=pnpm || ERRCODE=$?
+  npx @vue/cli create $ProjectName --no-git --inlinePreset "{\"useConfigFiles\": true,\"plugins\": {},\"vueVersion\": \"$1\"}" --packageManager=npm || ERRCODE=$?
 
   cd $ProjectName
   echo "installing local vue-cli-plugin-single-spa"
-  pnpm install -D ../../.. || ERRCODE=$?
+  npm install -D ../../.. || ERRCODE=$?
   echo "invoking vue-cli-plugin-single-spa"
   yes | npx @vue/cli invoke single-spa || ERRCODE=$?
   echo "creating vue.config.js"
   echo "module.exports={pluginOptions: {'single-spa': {outputSystemJS: $2}}}" > vue.config.js
   echo "building"
-  pnpm build || ERRCODE=$?
+  npm build || ERRCODE=$?
 
   exit $ERRCODE
 else
